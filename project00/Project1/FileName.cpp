efb7a6bc376b3ae3d68e6eb1187c4c4ad36e6f14
BiJiBen_Du <19231331237@163.com>
2026-04-18 17:00:35 +0800

    笔记本07

---
+++ project00/Project1/FileName.cpp
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+
+using namespace std;
+
+//4.2对象的初始化和清理
+
+
+//4.2.1构造函数和析构函数
+
+
+
+int main()
+{
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
