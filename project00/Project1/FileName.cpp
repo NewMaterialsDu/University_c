fa512ee1c3c20cb58549da499335ab639fca7e86
BiJiBen_Du <19231331237@163.com>
2026-05-21 18:56:04 +0800

    okok

---
+++ project00/Project1/FileName.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+
+using namespace std;
+
+//4.4сят╙
+
+
+
+int main()
+{
+   
+
+    system("pause");
+    return 0;
+}
