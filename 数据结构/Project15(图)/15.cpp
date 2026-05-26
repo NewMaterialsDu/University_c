d7b709124c5433278ac17aebc143dfa591d3de42
TaiShi_Du_C <1237@TaiShi_Du_C>
2026-05-26 16:51:00 +0800

    okok

--- 数据结构/Project15(图)/15.cpp
+++ 数据结构/Project15(图)/15.cpp
@@ -8,9 +8,28 @@ typedef int EdgeType;
 
 #define MAXSIZE 100
 
+typedef struct {
+	VertexType vertex[MAXSIZE];
+	EdgeType arr[MAXSIZE][MAXSIZE];
+	int vertex_num;
+	int edge_num;
+}Mat_Grph;
 
+int visited[MAXSIZE];
 
+void create_graph(Mat_Grph* G) {
+	G->vertex_num = 9;
+	G->edge_num = 15;
+	G->vertex[0] = 'A';
+	G->vertex[1] = 'B';
+	G->vertex[2] = 'C';
+	G->vertex[3] = 'D';
+	G->vertex[4] = 'E';
+	G->vertex[5] = 'F';
+	G->vertex[6] = 'G';
+	G->vertex[7] = 'H';
 
+}
 
 int main() {
 	
